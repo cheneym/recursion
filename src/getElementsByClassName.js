0e6dc4c18ddb0911bfcbc90dd32a4f73d54fd22d
Cheney Mao <cheneymao94@gmail.com>
2016-08-08 03:24:23 -0700

    Implement class comparison algorithm in getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  let targetClasses = className.split(' ');
+
+  var getElemByClassName = function (obj) {
+    let result = [];
+
+    let objClasses = obj.className.split(' ');
+    let hasTargetClasses = targetClasses.every(function(name, index) {
+      return (objClasses.indexOf(name) > -1);
+    });
+    
+    if (hasTargetClasses) {
+      result.push(obj);
+    }
+
+    return result;
+  };
+
+  return getElemByClassName(document.documentElement);
 };
