6b627f11d19796843f9b1cfd466d1f6e094ff84e
Cheney Mao <cheneymao94@gmail.com>
2016-08-19 05:26:13 -0700

    Parse JSON string representing a boolean or number in parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,27 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  if (typeof json === 'boolean' || typeof json === 'number' || json === null) {
+    return json;
+  }
+
+  let result;
+
+  if (json === 'true') {
+    result = true;
+  } else if (json === 'false') {
+    result = false;
+  } else {
+    switch (json[0]) {
+    case '{':
+      result = {};
+    case '[':
+      result = [];
+    case '"':
+      result = '';
+    default: //must be number
+      result = parseFloat(json);
+    }
+  }
+  return result;
 };
