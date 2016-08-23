11e24181c1f19eacd2876b59ddcaef2d3253c442
Cheney Mao <cheneymao94@gmail.com>
2016-08-22 22:13:29 -0700

    Add a function to disassemble string of obj in parseJSON
    extractFromObject takes a JSON string of an object and break up the
    string into JSON strings of properties and their corresponding values.
    The function itself is not yet used in the main program. It currently
    cannot handle invalid JSON strings.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -9,6 +9,71 @@ var parseJSON = function(json) {
 
   let result;
 
+  let extractFromArray = function(str) {
+
+  };
+
+  let extractFromObject = function(str) {
+    str = str.slice(1, str.length - 1);
+    let lookingForProp = true;
+    let stack = [];
+    let props = [];
+    let vals = [];
+    for (let i = 0; i < str.length; i++) {
+      switch (str[i]) {
+      case ':':
+        lookingForProp = false;
+        break;
+      case ',':
+        lookingForProp = true;
+        break;
+      case '{':
+      case '[':
+      case '"':
+        let char = str[i];
+        let pair = '"';
+        if (char === '[') {
+          pair = ']';
+        } else if (char === '{') {
+          pair = '}';
+        }
+        stack.push(char);
+
+        for (let j = i + 1; j < str.length; j++) {
+          if (str[j] === pair) {
+            stack.pop();
+          } else if (str[j] === char) {
+            stack.push(char);
+          } 
+          if (stack.length === 0) {
+            let item = str.slice(i, j + 1);
+            if (lookingForProp) {
+              props.push(item);
+            } else {
+              vals.push(item);
+            }
+            i = j;
+            break;
+          }
+        }
+        break;
+      default:
+        for (let j = i + 1; j < str.length; j++) {
+          if (str[j] === ',' || str[j] === '}') {
+            let item = str.slice(i, j);
+            if (lookingForProp) {
+              props.push(item);
+            } else {
+              vals.push(item);
+            }
+            i = j - 1;
+            break;
+          }
+        }
+      }
+    }
+  };
+
   if (json === 'true') {
     result = true;
   } else if (json === 'false') {
